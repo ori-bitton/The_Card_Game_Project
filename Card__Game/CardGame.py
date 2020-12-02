38e78778bdc9d7062365ee40ca62d005a652d03e
Ori Bitton <74710556+ori-bitton@users.noreply.github.com>
2020-12-02 11:05:06 +0200

    New Calss: CardGame. New Functions in Player: GT and EQ.

---
+++ Card__Game/CardGame.py
@@ -0,0 +1,25 @@
+from Card__Game.DeckOfCards import DeckOfCards
+from Card__Game.Player import Player
+
+
+class CardGame:
+
+    def __init__(self, name1, name2, hand = 10):
+        self.deck = DeckOfCards()
+        self.player1 = Player(str(name1), hand)
+        self.player2 = Player(str(name2), hand)
+        self.new_game()
+
+    def new_game(self):
+        self.deck.shuffle()
+        self.player1.set_hand(self.deck)
+        self.player1.set_hand(self.deck)
+
+    def get_winner(self):
+        if self.player1 > self.player2:
+            return self.player1
+        elif self.player2 > self.player1:
+            return self.player2
+        elif self.player1 == self.player2:
+            return
+
--- Card__Game/Player.py
+++ Card__Game/Player.py
@@ -10,18 +10,32 @@ class Player:
             self.numofcards = 26
         self.playerdeck = []
 
-    def set_hand(self):
-        maindeck = DeckOfCards()
+    def set_hand(self, maindeck):
+        if maindeck != DeckOfCards():
+            raise TypeError("Invalid Type, Must be DeckOfCards.")
         for i in range(self.numofcards):
             self.playerdeck.append(maindeck.deal_one())
 
     def get_card(self):
         self.playerdeck.pop(0)
 
-    def add_card(self):
-        maindeck = DeckOfCards()
+    def add_card(self, maindeck):
+        if maindeck != DeckOfCards():
+            raise TypeError("Invalid Type, Must be DeckOfCards.")
         self.playerdeck.append(maindeck.deal_one())
 
     def show(self):
         print(self.name)
         print(self.playerdeck)
+
+    def __gt__(self, other):
+        if len(self.playerdeck) > len(other.playerdeck):
+            return True
+        if len(self.playerdeck) < len(other.playerdeck):
+            return False
+
+    def __eq__(self, other):
+        if len(self.playerdeck) == len(other.playerdeck):
+            return True
+        else:
+            return False
