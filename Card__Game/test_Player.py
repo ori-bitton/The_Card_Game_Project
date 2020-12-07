9745c2acd4cc7682838cedebcc0f2c04d71c88cb
Amit Jonas <amitjonas1@gmail.com>
2020-12-07 15:40:16 +0200

    Finished game 1.2

--- Card__Game/test_Player.py
+++ Card__Game/test_Player.py
@@ -1,7 +1,8 @@
-from unittest import TestCase
+from unittest import TestCase,mock
 from Card__Game.Player import Player
 from Card__Game.DeckOfCards import DeckOfCards
 from Card__Game.Card import Card
+
 
 
 class TestPlayer(TestCase):
@@ -15,6 +16,11 @@ class TestPlayer(TestCase):
     def tearDown(self):
         print("TearDown")
 
+    def test___init__(self):
+        with self.assertRaises(IndexError):
+            self.player2.__init__("Ori", -1)
+            self.player2.__init__("Ori", 281)
+
     def test_set_hand(self):
         # Both players need to take their cards from the same deck.
         self.player1.set_hand(self.maindeck)
@@ -25,6 +31,9 @@ class TestPlayer(TestCase):
         self.assertEqual(len(self.player1.playerdeck), self.player1.numofcards)
         self.assertEqual(len(self.player2.playerdeck), self.player2.numofcards)
 
+
+
+
     def test_get_card(self):
         # The function takes a card from the player and presents it
         self.player1.set_hand(self.maindeck)
@@ -33,9 +42,7 @@ class TestPlayer(TestCase):
         self.assertNotIn(rand_card, self.player1.playerdeck)
 
         # Edge Cases
-        with self.assertRaises(IndexError):
-            self.player2.get_card()
-            self.player1.numofcards = -1
+        self.assertTrue(self.player2.get_card() == None) #Taking a card from an empty deck
         with self.assertRaises(TypeError):
             Player([1, 2, 3], {1: 10, 2: 20})
 
@@ -45,3 +52,14 @@ class TestPlayer(TestCase):
         self.player1.add_card(self.card1)
         self.assertEqual(len(self.player1.playerdeck), self.player1.numofcards)
         self.assertIn(self.card1, self.player1.playerdeck)
+
+        # Edge cases.
+        with self.assertRaises(TypeError):
+            self.player1.add_card(self.player2)
+
+    def test_show(self):
+        self.assertEqual(self.player1.show(),None)
+
+    def test___eq__(self):
+        self.player1.set_hand(self.maindeck)
+        self.assertNotEqual(len(self.player1.playerdeck),len(self.player2.playerdeck))
